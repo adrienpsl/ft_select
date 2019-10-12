2bc218163960347dbd3cf3f30f9cf69c49537cdf
adpusel <adrien.pusel@gmail.com>
2019-10-12 14:07:28 +0200

    fix the bug of double signal with the fg mode

--- src/ft_select.signals.c
+++ src/ft_select.signals.c
@@ -13,6 +13,8 @@
 # include <ft_select.h>
 #include <sys/ioctl.h>
 
+static int g_is_foreground;
+
 static void quit(int nb)
 {
 	t_sct *s;
@@ -22,40 +24,42 @@ static void quit(int nb)
 	(void)nb;
 }
 
-static void put_in_foreground(int nb)
-{
-	// here set a protection to that.
-	t_sct *s;
-
-	s = get_sct(NULL);
-	catch_all_signal();
-	ft_dprintf(0, "fore");
-	if (OK != set_canonical(&s->termios))
-	{
-		quit_binary(s->elements, &s->termios);
-	}
-//	get_window_and_print(s);
-	(void)nb;
-}
-
 static void put_in_background(int nb)
 {
-	ft_dprintf(0, "back");
 	unset_canonical_mode(&get_sct(NULL)->termios);
 	signal(SIGTSTP, SIG_DFL);
 	ioctl(1, TIOCSTI, "\x1A");
+	g_is_foreground = true;
 	(void)nb;
+}
+
+static void put_in_foreground(int nb)
+{
+	t_sct *s;
+
+	if (g_is_foreground == true)
+	{
+		s = get_sct(NULL);
+		signal(SIGTSTP, put_in_background);
+		if (OK != set_canonical(&s->termios))
+		{
+			quit_binary(s->elements, &s->termios);
+		}
+		//	get_window_and_print(s);
+		(void)nb;
+		g_is_foreground = true;
+	}
 }
 
 static void changing_window(int nb)
 {
 	(void)nb;
-//	get_window_and_print(get_sct(NULL));
-ft_dprintf(0, "change");
+	//	get_window_and_print(get_sct(NULL));
 }
 
 void catch_all_signal(void)
 {
+	g_is_foreground = false;
 	signal(SIGINT, quit);
 	signal(SIGHUP, quit);
 	signal(SIGQUIT, quit);
